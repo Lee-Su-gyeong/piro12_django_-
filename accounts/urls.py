6624e44ef426d464704ebea99254366adcb3933a
Lee-Su-gyeong <dune2011@naver.com>
2020-01-28 02:34:27 +0900

    암호 재설정 구현하기

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('login/<uidb64>/<token>/', views.login_via_url, name='login_via_url'),
 
     path('password_change/', views.MyPasswordChangeView.as_view(), name='password_change'),
-#   path('password_change/done/',
-#       auth_views.PasswordChangeDoneView.as_view(
 #           template_name='accounts/password_change_done.html'
 #       ),
 #   name='password_change_done'),
+
+    path('password_reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
